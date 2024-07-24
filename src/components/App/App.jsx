a0584e7b255d91c43be6a684565f48a543e4be79
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-24 12:07:37 +0500

    configured react-router, deleted unused files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Link, Route, Router, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
