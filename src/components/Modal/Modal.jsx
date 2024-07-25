f57b8f773b7af5e08d279d41ba57474595c6f2dc
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-25 18:03:59 +0500

    added hidden navigation

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,24 @@
-export default function Modal({ title, handleCloseModal }) {
+export default function Modal({ title, description, hiddenSection, handleCloseModal }) {
+  const buttonExtraClass = title === "Соревнования" || title === "Проекты"
+    ? "modal__button-know-more_hidden"
+    : "";  
 
   return (
-    <div className="modal-overlay" onClick={handleCloseModal}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="modal-close" onClick={handleCloseModal}></button>
+    <div className="modal__overlay" onClick={handleCloseModal}>
+      <div className="modal__container" onClick={(e) => e.stopPropagation()}>
+        <button className="modal__button-close" onClick={handleCloseModal}></button>
         <h2>{title}</h2>
-        <p>Содержимое модального окна.</p>
+        <p>{description}</p>
+        {hiddenSection.map(
+            (x, index) => (
+            <div key={index}>    
+            <h2 className="modal__title">{x.title}</h2>
+            <p className="modal__description">{x.description}</p>
+            </div>
+            )
+        )}
+        <button className="modal__button-send">Подать заявку</button>
+        <button className={`modal__button-know-more ${buttonExtraClass}`}>Узнать больше</button>
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
 import Modal from "../Modal/Modal";
 
-export default function Navbar({title}) {
+export default function Navbar({ title, description, hiddenSection }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
-  }
+  };
 
   const handleCloseModal = () => {
     setIsModalOpen(false);
+    console.log(hiddenSection)
   };
 
   return (
     // разметка для создания одной позиции меню для главной и лк
     <div className="navbar">
       <h2 className="navbar__title">{title}</h2>
-      <button type="button" className="navbar__button-link" onClick={handleOpenModal}></button>
-      {isModalOpen && <Modal title={title} handleCloseModal={handleCloseModal} />}
+      <button
+        type="button"
+        className="navbar__button-link"
+        onClick={handleOpenModal}
+      ></button>
+      {isModalOpen && (
+        <Modal
+          title={title}
+          description={description}
+          hiddenSection={hiddenSection}
+          handleCloseModal={handleCloseModal}
+        />
+      )}
     </div>
   );
 }
