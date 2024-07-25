2dfc148aca4d002ec4e0f249e8a7448b4b86cce3
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-25 15:55:44 +0500

    added modal

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import Modal from "../Modal/Modal";
+
 export default function Navbar({title}) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     // разметка для создания одной позиции меню для главной и лк
     <div className="navbar">
       <h2 className="navbar__title">{title}</h2>
-      <button type="button" className="navbar__button-link"></button>
+      <button type="button" className="navbar__button-link" onClick={handleOpenModal}></button>
+      {isModalOpen && <Modal title={title} handleCloseModal={handleCloseModal} />}
     </div>
   );
 }
