bb32b9e6d3c1106354cd8ca1a95c50fb3626fd6d
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-28 11:40:39 +0500

    added window with notification

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom'; 
+import './Header.css'; 
+import { useState } from 'react';
+import Notification from '../Notification/Notification';
+
+//добавить измнение иконки если получил уведомление и написать соот.функцию
+
 export default function Header() {
-  return (
-    <div className='header'>
-    <img src={require('../../images/header-logo.svg').default} />
-    <img src={require('../../images/notification.svg').default} />
-    </div>
-  );
-}
+    const [isNotificationOpen, setIsNotificationOpen] = useState(false); 
+    const handleNotificationClick = () => {
+      setIsNotificationOpen(!isNotificationOpen);
+      console.log(isNotificationOpen);
+    };
+  
+    const closeNotification = () => {
+      setIsNotificationOpen(false); 
+    };
+  
+    return (
+      <div className="header">
+        <Link
+          to="/account"
+          className="header__button header__button--logo"
+        ></Link>
+        <button
+          type="button"
+          className="header__button header__button--notification"
+          onClick={handleNotificationClick}
+        ></button>
+        {isNotificationOpen && (<Notification onClose={closeNotification} className={isNotificationOpen ? 'notification__show' : ''} />)}
+      </div>
+    );
+  }
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,29 @@
+export default function Notification({ onClose }) {
+  return (
+    <div className={`notification slide-in`}>
+      <div className="notification__container-title">
+        <button
+          type="button"
+          className="notification__button-close"
+          onClick={onClose}
+        ></button>
+        <h2 className="notification__title">Уведомления</h2>
+      </div>
+      <div className="notification__container">
+        <div className="notification__container-box">
+          {/* Захардкожено! Подвязать уведомления*/}
+          <div className="notification__container-box-flex">
+            <h3 className="notification__box-title">Видео-заявка</h3>
+            <p className="notification__box-data">3 дня назад</p>
+          </div>
+          <p className="notification__box-description">
+            Вы не прошли в 3 этап. Не расстраиваетесь, скоро начинаются новые
+            мероприятия
+          </p>
+        </div>
+        {/* Подвязать функцию удаления */}
+        <button className="notification__button-clean"></button>
+      </div>
+    </div>
+  );
+}
