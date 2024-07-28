db61255409344e129fc65a3643792ea5535d1891
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-28 12:16:54 +0500

    added style css modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -21,11 +21,13 @@ export default function Modal({ title, description, hiddenSection, handleCloseMo
   return (
     <div className="modal__overlay" onClick={handleCloseModal}>
       <div className="modal__container" onClick={(e) => e.stopPropagation()}>
-        <button className="modal__button-close" onClick={handleCloseModal}></button>
-        <h2>{title}</h2>
-        <p>{description}</p>
+       <div className="modal__container-flex">
+          <h2 className="modal__container-title">{title}</h2>
+          <button className="modal__button-close" onClick={handleCloseModal}></button>
+        </div>
+        <p className="modal__container-text">{description}</p>
         {hiddenSection.map((x, index) => (
-          <div key={index}>
+          <div className="modal__container-flex" key={index}>
             <h2 className="modal__title">{x.title}</h2>
             <button onClick={() => toggleSection(index)} className="modal__button-toggle">
               {openSections[index] ? "Скрыть" : "Показать"}
