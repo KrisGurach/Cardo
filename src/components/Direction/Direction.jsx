4900fad8f013aa15e869e84194914c019b32cabf
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-30 11:15:07 +0500

    added button back

--- src/components/Direction/Direction.jsx
+++ src/components/Direction/Direction.jsx
@@ -15,13 +15,14 @@ export default function Direction({ images, description, hiddenSection }) {
 
   return (
     <section className="direction">
+      <button className="direction__back-button" onClick={() => window.history.back()}></button>
       <img className="direction__image" src={images} alt="" />
       <p className="direction__text">{description}</p>
       <div className="direction__container">
         {hiddenSection.map((x, index) => (
           <div key={index}>
             <div className="modal__container-flex">
-              <h2 className={`modal__title ${index === hiddenSection.length - 1 ? 'title_gray' : ''}`}>{x.title}</h2>
+              <h2 className="modal__title">{x.title}</h2>
               <button
                 onClick={() => toggleSection(index)}
                 className={`modal__button-toggle ${
