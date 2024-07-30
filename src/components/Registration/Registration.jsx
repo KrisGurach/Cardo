b250068e767c86874760a6c9aec8a9df9f9d6b5f
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-30 19:03:18 +0500

    style css regustration

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,51 +1,74 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
+import vkIcon from '../../images/icon-vk.svg';
+import googleIcon from '../../images/icon-google.svg';
+import appleIcon from '../../images/icon-apple.svg';
 
 export default function Registration() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-    <div className="registration-form">
-      <img src={require('../../images/logotype.svg')} alt="логотип, возврат на главную страницу" />
-      <h3>Создать аккаунт</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">E-mail:</label>
+    <div className="form">
+      <img
+        className="form__logo"
+        src={require("../../images/logotype.png")}
+        alt="логотип, возврат на главную страницу"
+      />
+      <h3 className="form__title">Создать аккаунт</h3>
+      <form className="form__form" onSubmit={handleSubmit}>
+        <div className='form__field'>
           <input
+            className="form__input"
             type="email"
             id="email"
+            placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label htmlFor="password">Пароль:</label>
+        <div className='form__field'>
           <input
+            className="form__input"
             type="password"
             id="password"
+            placeholder="Пароль"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label htmlFor="confirm-password">Подтвердите Пароль:</label>
+        <div className='form__field'>
           <input
+            className="form__input"
             type="password"
             id="confirm-password"
+            placeholder="Подтвердите пароль"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
         </div>
-        <button type="submit">Зарегистрироваться</button>
+        <div className='form__container-icon'>
+          <button className='form__button-by-network'><img src={vkIcon} alt="регистрация через ВКонтакте" /></button>
+          <button className='form__button-by-network'><img src={googleIcon} alt="регистрация через Google" /></button>
+          <button className='form__button-by-network'><img src={appleIcon} alt="регистрация через Apple" /></button>
+        </div>
+        <button className="form__button" type="submit">
+          Зарегистрироваться
+        </button>
       </form>
+      <p className="registration__text">
+        Создавая аккаунт ты соглашаешься с <a href="#" className="registration__link">
+          условиями и политикой конфиденциальности.
+        </a>
+      </p>
     </div>
   );
 }
