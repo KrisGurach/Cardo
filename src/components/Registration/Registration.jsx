1ad2a849fc790960df47ae2a1a8f823a0b9abeb9
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-30 20:22:43 +0500

    added toggle visibility password

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import vkIcon from '../../images/icon-vk.svg';
@@ -6,8 +7,9 @@ import appleIcon from '../../images/icon-apple.svg';
 
 export default function Registration() {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,7 +24,7 @@ export default function Registration() {
       />
       <h3 className="form__title">Создать аккаунт</h3>
       <form className="form__form" onSubmit={handleSubmit}>
-        <div className='form__field'>
+        <div className="form__field">
           <input
             className="form__input"
             type="email"
@@ -33,39 +35,64 @@ export default function Registration() {
             required
           />
         </div>
-        <div className='form__field'>
+        <div className="form__field">
           <input
             className="form__input"
-            type="password"
+            type={showPassword ? "text" : "password"}
             id="password"
             placeholder="Пароль"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
+          <button
+            className={
+              showPassword ? "form__button-hidden" : "form__button-show"
+            }
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+          ></button>
         </div>
-        <div className='form__field'>
+        <div className="form__field">
           <input
             className="form__input"
-            type="password"
+            type={showPassword ? "text" : "password"}
             id="confirm-password"
             placeholder="Подтвердите пароль"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
+          <button
+            className={
+              showPassword ? "form__button-hidden" : "form__button-show"
+            }
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+          ></button>
         </div>
-        <div className='form__container-icon'>
-          <button className='form__button-by-network'><img src={vkIcon} alt="регистрация через ВКонтакте" /></button>
-          <button className='form__button-by-network'><img src={googleIcon} alt="регистрация через Google" /></button>
-          <button className='form__button-by-network'><img src={appleIcon} alt="регистрация через Apple" /></button>
+        <div className="form__container-icon">
+          <button className="form__button-by-network">
+            <img src={vkIcon} alt="регистрация через ВКонтакте" />
+          </button>
+          <button className="form__button-by-network">
+            <img src={googleIcon} alt="регистрация через Google" />
+          </button>
+          <button className="form__button-by-network">
+            <img src={appleIcon} alt="регистрация через Apple" />
+          </button>
         </div>
         <button className="form__button" type="submit">
           Зарегистрироваться
         </button>
       </form>
       <p className="registration__text">
-        Создавая аккаунт ты соглашаешься с <a href="#" className="registration__link">
+        Создавая аккаунт ты соглашаешься с{" "}
+        <a
+          href="https://kardoaward.com/privacy-policy/"
+          className="registration__link"
+          target="_blank"
+        >
           условиями и политикой конфиденциальности.
         </a>
       </p>
