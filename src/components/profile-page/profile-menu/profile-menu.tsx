c3a5a7f17251df6fef2ba31ad7e5ac491de37a71
Danbka-Taranbka <danila.klim01@gmail.com>
2024-08-01 23:43:09 -0600

    routes added

--- src/components/profile-page/profile-menu/profile-menu.tsx
+++ src/components/profile-page/profile-menu/profile-menu.tsx
@@ -2,11 +2,12 @@ import { FC } from "react";
 import React from "react";
 import style from './style.module.css';
 import MenuItem from "../menu-item/menu-item";
+import { eventsRoute } from "../../../constants/routes";
 
 const ProfileMenu:FC = () =>  {
   return (
     <ul className={style.container}>
-      <MenuItem link='#' title="Мероприятия"></MenuItem>
+      <MenuItem link={eventsRoute} title="Мероприятия"></MenuItem>
       <MenuItem link='#' title="Мои видео"></MenuItem>
       <MenuItem link='#' title="Мой профиль"></MenuItem>
       <MenuItem link='#' title="Настройки"></MenuItem>
